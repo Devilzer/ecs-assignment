10cdc7f6d10192bad223958cca75a0c9d079c4f1
Devilzer <jenadeepak724@gmail.com>
2021-01-22 00:57:03 +0530

    add front header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './style/App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className= "header">
+        <div className="heading">
+          Book Store
+        </div>
+        <input type="text" placeholder="Search Book...."/>
+        <div className ="head-buttons">
+          <div className="cart" >
+            <span>0</span><i className="fab fa-opencart"></i>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
