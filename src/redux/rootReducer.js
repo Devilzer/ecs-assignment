59e2bb05d91710f7ad2639f6d2387d7b551629d0
Devilzer <jenadeepak724@gmail.com>
2021-01-22 04:39:02 +0530

    add redux-persist

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,10 +3,19 @@ import uiReducer from "./reducers/uiReducer";
 import productReducer from "./reducers/productReducer";
 import cartReducer from "./reducers/cartReducer";
 
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 const rootReducer = combineReducers({
     ui : uiReducer,
     product : productReducer,
     cart : cartReducer,
 });
+const persistConfig = {
+    key : "root",
+    storage,
+};
 
-export default rootReducer;
+const persistedReducer = persistReducer(persistConfig,rootReducer);
+
+export default persistedReducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,9 @@ import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./rootReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
+import { persistStore } from "redux-persist";
 
 const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(thunk)));
+const persistor = persistStore(store);
 
-export default store;
+export {store,persistor};
